3d6144b96b2c67c64f89a91e5e640aed240e2a7e
Kohei YOSHIDA <kohei@yosida95.com>
2022-04-13 03:02:40 +0900

    update author email

--- setup.py
+++ setup.py
@@ -15,20 +15,22 @@ def _read(name):
     except:
         return ""
 readme = _read("README.rst")
-license = _read("LICENSE.rst")
 
 setup(
     name='jsonrpc',
     version='0.1.1',
-    author='Kohei YOSHIDA',
-    author_email='license@yosida95.com',
     description='A JSON RPC client library.',
     long_description=readme,
-    license=license,
     url='https://github.com/yosida95/python-jsonrpc',
+
+    author='Kohei YOSHIDA',
+    author_email='kohei@yosida95.com',
+    license='BSD-3-Clause',
+
     packages=find_packages(),
     install_requires=requires,
     tests_require=tests_require,
+
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
